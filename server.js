f934246a5e25791c7884ef611e726c403d404aa8
VarestttMC <varestx2yt@gmail.com>
2026-05-16 15:30:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const net = require("net");
+const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("MC pinger is running");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server running");
+});
+
+// ping loop
 setInterval(() => {
-    const socket = new net.Socket();
+  const socket = new net.Socket();
 
-    socket.connect(25565, "orbitlaunchpad.xyz", () => {
-        console.log("Pinged");
-        socket.destroy();
-    });
+  socket.connect(25565, "orbitlaunchpad.xyz", () => {
+    console.log("Pinged server");
+    socket.destroy();
+  });
 
-    socket.on("error", () => {});
+  socket.on("error", () => {});
 }, 300000);
