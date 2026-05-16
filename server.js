56221ad1b1bd9db65c9989b26d411f65bb916e60
VarestttMC <varestx2yt@gmail.com>
2026-05-16 15:21:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-{
-  "name": "mc-pinger",
-  "version": "1.0.0",
-  "main": "server.js"
-}
+const net = require("net");
+
+setInterval(() => {
+    const socket = new net.Socket();
+
+    socket.connect(25565, "orbitlaunchpad.xyz", () => {
+        console.log("Pinged");
+        socket.destroy();
+    });
+
+    socket.on("error", () => {});
+}, 300000);
